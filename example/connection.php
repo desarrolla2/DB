7e84570dddbf8a3c2ee5b88e2238e9ab46530f93
Daniel González Cerviño <daniel.gonzalez@freelancemadrid.es>
2012-09-05 22:58:27 +0200

    connection example

---
+++ example/connection.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the DB proyect.
+ * 
+ * Description of connection
+ *
+ * @author : Daniel González <daniel.gonzalez@freelancemadrid.es> 
+ * @file : connection.php , UTF-8
+ * @date : Sep 5, 2012 , 10:57:01 PM
+ */
+require __DIR__ . '/../vendor/autoload.php'; //generate by composer
+
+use Desarrolla2\DB\DB;
+use Desarrolla2\DB\Adapter\MySQL;
+
+$db = new DB;
+$db->setAdaper(new MySQL);
+
+$db->setOptions(array(
+    'database' => '', 'username' => '',
+    'hostname' => '', 'userpass' => ''
+));
+
+$db->connect();
