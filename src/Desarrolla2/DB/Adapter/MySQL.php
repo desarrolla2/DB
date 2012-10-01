d6ee5e4c8e21a21dcf0e1302e2f26b638731cf7e
Daniel González Cerviño <daniel.gonzalez@ideup.com>
2012-10-01 12:19:15 +0200

    fix options valids

--- src/Desarrolla2/DB/Adapter/MySQL.php
+++ src/Desarrolla2/DB/Adapter/MySQL.php
@@ -202,7 +202,7 @@ class MySQL implements AdapterInterface
         $this->dropDatabase();
         $this->createDatabase();
         $cmd = 'mysql -u ' . $this->options['username'] . ' -p\'' . $this->options['password'] . '\'' .
-                ' -h ' . $this->options['host'] . ' ' . $this->options['database'] .
+                ' -h ' . $this->options['hostname'] . ' ' . $this->options['database'] .
                 ' --verbose --quick < ' . $file_name;
         echo $cmd . PHP_EOL;
         return exec($cmd);
@@ -216,7 +216,7 @@ class MySQL implements AdapterInterface
     public function dump($filename)
     {
         $cmd = 'mysqldump -u ' . $this->options['username'] . ' -p\'' . $this->options['password'] . '\'' .
-                ' -h ' . $this->options['host'] . ' ' . $this->options['database'] .
+                ' -h ' . $this->options['hostname'] . ' ' . $this->options['database'] .
                 ' --add-drop-table --add-locks --create-options --disable-keys --extended-insert ' .
                 ' --quick --set-charset --compress --verbose --no-create-db > ' . //--compatible=mysql40
                 $file_name;
